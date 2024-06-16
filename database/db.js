7a4eee21515935723b64fc1b4570263033c1cdc5
Caravieri <caravierigui14@gmail.com>
2024-06-16 15:53:31 -0300

    teste de conexão

--- database/db.js
+++ database/db.js
@@ -1,8 +1,16 @@
 import 'dotenv/config'
 import postgres from 'postgres'
 
-const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
+let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
 
-const URL = `postgres://${PGHOST}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?options=project%3d${ENDPOINT_ID}`
-
-export const sql = postgres(URL, { ssl: "require"})
+export const sql = postgres({
+  host: PGHOST,
+  database: PGDATABASE,
+  username: PGUSER,
+  password: PGPASSWORD,
+  port: 5432,
+  ssl: 'require',
+  connection: {
+    options: `project=${ENDPOINT_ID}`,
+  },
+});
